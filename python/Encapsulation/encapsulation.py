908263366ffeebdf4740ddb5b13e4ae188c2972f
Virus-777 <ragus2006@gmail.com>
2021-09-14 12:14:10 +0530

    encapsulation added

---
+++ python/Encapsulation/encapsulation.py
@@ -0,0 +1,43 @@
+
+"""
+Encapsulation
+Encapsulation is one of the fundamental concepts in object-oriented programming (OOP).
+It describes the idea of wrapping data and the methods that work on data within one unit.
+This puts restrictions on accessing variables and methods directly and can prevent the accidental modification of data.
+To prevent accidental change, an object’s variable can only be changed by an object’s method. Those types of variables are known as private variables.
+
+A class is an example of encapsulation as it encapsulates all the data that is member functions, variables, etc.
+"""
+
+
+# Python program to
+# demonstrate private members
+
+# Creating a Base class
+class Base:
+    def __init__(self):
+        self.a = "GeeksforGeeks"
+        self.__c = "GeeksforGeeks"
+
+
+# Creating a derived class
+class Derived(Base):
+    def __init__(self):
+        # Calling constructor of
+        # Base class
+        Base.__init__(self)
+        print("Calling private member of base class: ")
+        print(self.__c)
+
+
+# Driver code
+obj1 = Base()
+print(obj1.a)
+
+# Uncommenting print(obj1.c) will
+# raise an AttributeError
+
+# Uncommenting obj2 = Derived() will
+# also raise an AtrributeError as
+# private member of base class
+# is called inside derived class
